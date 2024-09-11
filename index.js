bd55d972a400d80bb3056c6e6af3d2a2adbd1f76
mirazfaruk <mirazfaruk@gmail.com>
2024-09-12 00:58:11 +0600

    Take data from client side and store in the database

--- index.js
+++ index.js
@@ -24,12 +24,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const touristSpotCollection = client.db('touristSpotDB').collection('touristSpot');
+
+        // Add new tourist spot
+        app.post('/add-tourist-spot', async (req, res) => {
+            const newSpot = req.body;
+            console.log(newSpot);
+            const result = await touristSpotCollection.insertOne(newSpot);
+            res.send(result);
+        });
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
