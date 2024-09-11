0996c64cefa1b2bcde57b6725e50a6e333484fcf
mirazfaruk <mirazfaruk@gmail.com>
2024-09-12 03:22:24 +0600

    Display spot details for specific id's

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5001;
 
@@ -33,6 +33,15 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        // Get specific spot using id
+        app.get('/tourist-spot/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await touristSpotCollection.findOne(query);
+            res.send(result);
+        });
+
 
         // Add new tourist spot
         app.post('/tourist-spot', async (req, res) => {
